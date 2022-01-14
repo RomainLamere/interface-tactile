8b7d7a16271fa6933c63d133f77715d3e568b38b
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2022-01-14 15:08:26 +0100

    fichiers pour la communication

---
+++ Assets/Script/WSCommunication.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WSCommunication : MonoBehaviour
+{
+    WebSocket ws;
+    private void Start()
+    {
+        ws = new WebSocket("ws://localhost:8080");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+        };
+    }
+    private void Update()
+    {
+        if (ws == null)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ws.Send("Hello");
+        }
+    }
+}
+}
