fa852ecbdad3bcea2d9d9ff6da48fa1cfa004241
Nakutaka <alexandre.col@etu.univ-cotedazur.fr>
2022-01-21 14:47:13 +0100

    Input on the screen to send data to the phones

--- VueJS/screen/src/components/SideMenu.vue
+++ VueJS/screen/src/components/SideMenu.vue
@@ -9,7 +9,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'SideMenu'
+};
 </script>
 
 <style lang="css" scoped>
--- VueJS/screen/src/components/Workspace.vue
+++ VueJS/screen/src/components/Workspace.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>Message: {{ msg }}</div>
+  <div>
+    Message: {{ msg }}
+    <input type="number" v-model="bpm">
+    <button @click="sendBPM()">Send new BPM</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Workspace",
   data(){
-      return {
-          msg: ''
-      }
+    return {
+      msg: '',
+      bpm: 0
+    }
   },
   sockets:{},
+  methods:{
+    sendBPM(){
+      console.log('Send BPM');
+      this.$socket.emit('newBPM',this.bpm);
+    }
+  },
   created() {
     this.$options.sockets.eventToScreen = (data) => {
       console.log(`Test subscribe: ${data}`);
