b2fd6c959a2730868fea3522a421160323ee6c41
Nakutaka <alexandre.col@etu.univ-cotedazur.fr>
2022-01-26 11:45:33 +0100

    Fast light color selector in the Screen

--- VueJS/screen/src/components/Workspace.vue
+++ VueJS/screen/src/components/Workspace.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="workspace-container">
-    Message: {{ msg }}
-    <input type="number" v-model="bpm">
-    <button @click="sendBPM()">Send new BPM</button>
+    <div>
+      Message: {{ msg }}
+      <input type="number" v-model="bpm">
+      <button @click="sendBPM()">Send new BPM</button>
+    </div>
+    <div class="color-picker">
+      <input type="color" name="color" id="color" v-model="color">
+      {{color}}
+      <button @click="sendColor()">Send new color</button>
+    </div>
     <div class="tracks">
       <Track></Track>
     </div>
@@ -21,7 +28,8 @@ export default {
   data(){
     return {
       msg: '',
-      bpm: 0
+      bpm: 0,
+      color: '#000000'
     }
   },
   sockets:{},
@@ -29,6 +37,28 @@ export default {
     sendBPM(){
       console.log('Send BPM');
       this.$socket.emit('newBPM',this.bpm);
+    },
+    sendColor(){
+      console.log('Send color', this.hexToRGB(this.color));
+      this.$socket.emit('changeLights',this.color)
+    },
+    hexToRGB(h){
+      let r = 0, g = 0, b = 0;
+
+      // 3 digits
+      if (h.length == 4) {
+        r = "0x" + h[1] + h[1];
+        g = "0x" + h[2] + h[2];
+        b = "0x" + h[3] + h[3];
+
+      // 6 digits
+      } else if (h.length == 7) {
+        r = "0x" + h[1] + h[2];
+        g = "0x" + h[3] + h[4];
+        b = "0x" + h[5] + h[6];
+      }
+      
+      return {r: +r, g: +g, b: +b};
     }
   },
   created() {
@@ -44,6 +74,10 @@ export default {
   .workspace-container{
     width: 100%;
     overflow: hidden;
+  }
+
+  .color-picker{
+    color: white;
   }
 
   .tracks{
