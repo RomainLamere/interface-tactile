68483c78e126e4545efd1f62fbe5146c911525ee
RomainLamere <54950366+RomainLamere@users.noreply.github.com>
2022-02-05 20:17:28 +0100

    fixing bug with open menu perfab

--- Unity/Assets/Script/canvasScript.cs
+++ Unity/Assets/Script/canvasScript.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using static touchLocation;
+
 
 public class canvasScript : MonoBehaviour, IPointerEnterHandler
 {
-
-    private float time = 0;
-    private bool isClicked = false;
     public GameObject prefabMenu;
     private Vector2 clicPosition;
     public EventSystem eventSystem;
     private PointerEventData myEventData;
+    public List<touchLocation> touches = new List<touchLocation>();
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +22,54 @@ public class canvasScript : MonoBehaviour, IPointerEnterHandler
     // Update is called once per frame
     void Update()
     {
+        int i = 0;
 
-        if (isClicked && time < 1)
+
+            print("yo "+Input.touchCount);
+            print(myEventData.pointerCurrentRaycast.gameObject.name);
+
+        while (i < Input.touchCount)
         {
-            time += Time.deltaTime;
-        }
-        if (time >= 1 && isClicked)
-        {
-            GameObject newMenu =  Instantiate(prefabMenu,this.transform);
-            newMenu.transform.position = new Vector3(clicPosition.x, clicPosition.y, 0);
-            newMenu.GetComponent<DragObject>().canvas = GetComponentInParent<Canvas>();
-            OnMouseUp();
-            HandleMenuRotation(newMenu);
+            Touch t = Input.GetTouch(i);
+            if (t.phase == TouchPhase.Began)
+            {
+                Debug.Log("touch began");
+
+                touches.Add(new touchLocation(t.fingerId, t.position));
+            }
+            else if (t.phase == TouchPhase.Ended)
+            {
+                Debug.Log("touch ended");
+                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId);
+                touches.RemoveAt(touches.IndexOf(thisTouch));
+            }
+            else
+            {
+                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId);
+                if (thisTouch != null)
+                {
+                    thisTouch.UpdateTimer();
+                    if (thisTouch.timer > 1)
+                    {
+                        GameObject newMenu = Instantiate(prefabMenu, this.transform);
+                        newMenu.GetComponent<RectTransform>().localPosition = getTouchPosition(thisTouch.position);
+                        newMenu.GetComponent<DragObject>().canvas = GetComponentInParent<Canvas>();
+                        clicPosition = thisTouch.position;
+                        HandleMenuRotation(newMenu);
+                        touches.RemoveAt(touches.IndexOf(thisTouch));
+                    }
+                }
+            }
+            ++i;
+
         }
     }
-
+    Vector2 getTouchPosition(Vector2 touchPosition)
+    {
+        print(touchPosition.x);
+        print(touchPosition.y);
+        return new Vector3(touchPosition.x-1920/2, touchPosition.y-1080/2, transform.position.z);
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         myEventData = eventData;
@@ -44,23 +77,14 @@ public class canvasScript : MonoBehaviour, IPointerEnterHandler
 
     public void OnMouseDown()
     {
-        if (eventSystem.IsPointerOverGameObject() == false || myEventData.pointerCurrentRaycast.gameObject.name == "background")
-        {
-            clicPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); ;
-            time = 0;
-            isClicked = true;
-        }
-    }
-    public void OnMouseUp()
-    {
-        isClicked = false;
+
     }
 
     private void HandleMenuRotation(GameObject menu)
     {
         float cameraWidth = Camera.main.scaledPixelWidth;
         float cameraHeight = Camera.main.scaledPixelHeight;
-        if(clicPosition.y > 0)
+        if(clicPosition.y > 540)
         {
             menu.GetComponent<RectTransform>().Rotate(0f, 0f, 180f);
         }
