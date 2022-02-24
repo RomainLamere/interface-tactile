680f4f02b688adfa256a127f2d28c441ed6b1511
YannLeon <54958888+YannLeon@users.noreply.github.com>
2022-02-24 18:49:17 +0100

    add a timer + refacto

--- ReactNative/studyophone/Record.js
+++ ReactNative/studyophone/Record.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {
     SafeAreaView,
     ScrollView,
@@ -12,6 +12,7 @@ import {
 import AudioRecord from 'react-native-audio-record';
 import {UserContext} from "./UserContext";
 import {readFile} from 'react-native-fs';
+import StopWatch from "react-native-stopwatch-timer/lib/stopwatch";
 
 export const Record = () =>{
     const [userContext, setUserContext] = useContext(UserContext);
@@ -21,6 +22,35 @@ export const Record = () =>{
         //console.debug(data);
         setSound(sound + data);
     });
+    const [timerInPause, setTimerInPause] = useState(true)
+    const [isRecord,setIsRecord] =  useState(false)
+    const [currentTimerCount, setCurrentTimerCount] = useState(0)
+
+    useEffect(()=>{
+            if(!timerInPause){
+
+                    setInterval(()=>{setCurrentTimerCount(currentTimerCount+10)},10)
+
+            }
+        },
+        [timerInPause])
+    const changeTimerPause=()=>{
+        if(timerInPause){
+            setTimerInPause(false)
+        }
+        else {
+            setTimerInPause(true)
+            setCurrentTimerCount(0)
+        }
+    }
+    const transformTimer = (timer)=>{
+        let sec = Math.floor(timer/1000)
+        let mil=timer-1000*sec
+        if(sec<10){
+            sec='0'+sec
+        }
+        return(sec+':'+mil)
+    }
     const options = {
         sampleRate: 16000, // default 44100
         channels: 1, // 1 or 2, default 1
@@ -34,26 +64,31 @@ export const Record = () =>{
     }
     return(
         <View style={{height:"100%"}}>
+            <Text>Current Record Time :</Text>
+            <StopWatch msecs start={!timerInPause}/>
             <Button
-                title={'record'}
+                title={isRecord?"stop":'record'}
                 onPress={() => {
-                    AudioRecord.init(options);
-                    AudioRecord.start();
-                }}
-            />
-            <Button
-                title={'stop'}
-                onPress={() => {
-                    AudioRecord.stop().then(r => {
-                        console.log(r);
-                        getUriToBase64(r).then(r2 => {
-                            console.log(sound);
-                            const chunk = Buffer.from(r2, 'base64');
-                            //Uint8Array.from(atob(base64_string), c => c.charCodeAt(0))
-                            userContext.socket.emit('voiceFromPhone', chunk);
-                            setSound('');
+
+                    if(!isRecord) {
+                        changeTimerPause()
+                        AudioRecord.init(options);
+                        AudioRecord.start();
+                    }
+                    else {
+                        changeTimerPause()
+                        AudioRecord.stop().then(r => {
+                            console.log(r);
+                            getUriToBase64(r).then(r2 => {
+                                console.log(sound);
+                                const chunk = Buffer.from(r2, 'base64');
+                                //Uint8Array.from(atob(base64_string), c => c.charCodeAt(0))
+                                userContext.socket.emit('voiceFromPhone', chunk);
+                                setSound('');
+                            });
                         });
-                    });
+                    }
+                    setIsRecord(!isRecord)
                 }}
             />
         </View>
